6cdc93576d9ce6cc0cb07b73df2e1e84216bde28
alex17-T <a.senatore151@studenti.unisa.it>
2025-03-25 14:26:57 +0100

    ahahha

--- AssignmentConcurrency/src/main/java/module-info.java
+++ AssignmentConcurrency/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.assignmentconcurrency {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.assignmentconcurrency to javafx.fxml;
     exports com.mycompany.assignmentconcurrency;
