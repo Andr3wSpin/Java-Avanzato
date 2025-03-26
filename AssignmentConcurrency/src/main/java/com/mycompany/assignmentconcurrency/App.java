98e3278127e7da0b1e434aeb43fa9e1b83c6b0bd
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-03-26 08:16:04 +0100

    .

--- AssignmentConcurrency/src/main/java/com/mycompany/assignmentconcurrency/App.java
+++ AssignmentConcurrency/src/main/java/com/mycompany/assignmentconcurrency/App.java
@@ -17,20 +17,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        
+        MPView mpView = new MPView
+        
         stage.setScene(scene);
         stage.show();
     }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+    
     public static void main(String[] args) {
         launch();
     }
