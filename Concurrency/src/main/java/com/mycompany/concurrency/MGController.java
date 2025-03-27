c7e50b3036519bab3732f32494ec30919cc1de3b
alex17-T <a.senatore151@studenti.unisa.it>
2025-03-27 12:01:57 +0100

    css schemrta di gico

--- Concurrency/src/main/java/com/mycompany/concurrency/MGController.java
+++ Concurrency/src/main/java/com/mycompany/concurrency/MGController.java
@@ -1,12 +1,39 @@
 package com.mycompany.concurrency;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MGController {
 
+        @FXML
+    private Label LBLTempo;
+
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
+    private Button BTNFatto;
+
+    @FXML
+    private Label LBLRestanti;
+
+    @FXML
+    private Label LBLDomanda;
+
+    @FXML
+    private TextField TXFRisposta;
+  
+    
+     public void initialize(URL location, ResourceBundle resources) {
+        
+    
+   
+     
+     }
+    
+    
+    
+    
 }
