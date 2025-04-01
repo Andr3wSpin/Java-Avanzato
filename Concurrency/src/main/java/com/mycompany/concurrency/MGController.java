3b3ace59a2e1b12d14dab555200d493175f4cf1a
alex17-T <a.senatore151@studenti.unisa.it>
2025-04-01 14:08:08 +0200

    elenco ag

--- Concurrency/src/main/java/com/mycompany/concurrency/MGController.java
+++ Concurrency/src/main/java/com/mycompany/concurrency/MGController.java
@@ -2,14 +2,18 @@ package com.mycompany.concurrency;
 
 import com.mycompany.concurrency.model.Domanda;
 import com.mycompany.concurrency.model.Partita;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
+import javafx.animation.ScaleTransition;
 import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +23,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -49,12 +58,23 @@ public class MGController {
     private Circle timerGrafico;
     
     private final double circ = 2*Math.PI*68;    
+    @FXML
+    private Rectangle animazioneOnda;
+    private  DropShadow shadow;
+    
+    
+    
+    
     public void setPartita(Partita partita) {
         
         this.partita = partita;
         
         domande = partita.getDomande();
         
+        //collego il bottone all suo rettangolo per l animazione
+        animazioneOnda.translateYProperty().bind(BTNFatto.translateYProperty());
+          shadow = new DropShadow();
+         shadow.setColor(Color.rgb(0, 0, 0, 0.3));
         setDomanda();
     }
    
@@ -125,8 +145,8 @@ public class MGController {
     @FXML
     private void avanza() {
         
+        animazioneClick();
         String risposta = TXFRisposta.getText();
-        
         domande.get(indiceDomanda - 1).setRisposta(risposta);
         
         setDomanda();
@@ -150,6 +170,164 @@ public class MGController {
             
             System.out.println("Erroer durante la generazione della nuova schermata!");
         }
+   
+     }
+    
+    public void animazioneClick(){
+    
+        
+      
+       
+         ScaleTransition allarga = new ScaleTransition(Duration.seconds(0.3),animazioneOnda);
+        allarga.setToX(1.7);
+        allarga.setToY(3.5);
+        allarga.play();
+        FadeTransition scompari = new FadeTransition(Duration.seconds(0.2),animazioneOnda);
+        scompari.setToValue(0);
+        scompari.play();
+        
+               
+        
+        allarga.setOnFinished( e-> {
+        
+            animazioneOnda.setScaleX(1);
+            animazioneOnda.setScaleY(1);
+            animazioneOnda.setOpacity(0.7);
+            
+        
+        });
+        
+               
     }
+
+    @FXML
+    private void onMouseExitedAnimation(MouseEvent event) {
+         TranslateTransition click = new TranslateTransition(Duration.seconds(0.2),BTNFatto);
+         click.setToY(5);
+         click.play();
+         BTNFatto.setEffect(null);
+         
+
+   
+    }
+
+    @FXML
+    private void onMousePressedAnimation(MouseEvent event) {
+        TranslateTransition click = new TranslateTransition(Duration.seconds(0.2),BTNFatto);
+        click.setToY(5);
+        click.setAutoReverse(true);
+        click.setCycleCount(2);
+        click.play();
+        
+           Timeline timeline = new Timeline( new KeyFrame(Duration.ZERO,  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(10);
+        shadow.setRadius(20);
+        BTNFatto.setEffect(shadow);
+        }),
+      
+                    new KeyFrame(Duration.seconds(0.1),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(8);
+        shadow.setRadius(17);
+        BTNFatto.setEffect(shadow);
+       
+       }),
+                    new KeyFrame(Duration.seconds(0.1),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(6);
+        shadow.setRadius(13);
+        BTNFatto.setEffect(shadow);
+       
+       }),
+       new KeyFrame(Duration.seconds(0.1),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(4);
+        shadow.setRadius(10);
+        BTNFatto.setEffect(shadow);
+       
+       }),
+      
+       new KeyFrame(Duration.seconds(0.2),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(3);
+        shadow.setRadius(7);
+        BTNFatto.setEffect(shadow);
+       
+       })
+      
+      );
+      
+         timeline.setCycleCount(2);
+         timeline.setAutoReverse(true);
+        timeline.play();
+       
+   
+        
+    }
+
+    @FXML
+    private void onMouseEntredAnimation(MouseEvent event) {
+        
+      Timeline timeline = new Timeline( new KeyFrame(Duration.ZERO,  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(0);
+        shadow.setRadius(0);
+        BTNFatto.setEffect(shadow);
+        }),
+       new KeyFrame(Duration.seconds(0.1),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(4);
+        shadow.setRadius(10);
+        BTNFatto.setEffect(shadow);
+       
+       }),
+       new KeyFrame(Duration.seconds(0.13),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(6);
+        shadow.setRadius(10);
+        BTNFatto.setEffect(shadow);
+       
+       }),
+       new KeyFrame(Duration.seconds(0.16),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(8);
+        shadow.setRadius(10);
+        BTNFatto.setEffect(shadow);
+       
+       }),
+       new KeyFrame(Duration.seconds(0.2),  t->{
+        
+        shadow.setOffsetX(0);
+        shadow.setOffsetY(10);
+        shadow.setRadius(20);
+        BTNFatto.setEffect(shadow);
+       
+       })
+      
+      );
+      
+      
+        timeline.play();
+       
+        TranslateTransition hover= new TranslateTransition(Duration.seconds(0.2),BTNFatto);
+          hover.setToY(-5);
+          hover.play();
+    }
+
+
+   
+    
+ 
+    
 }
     
