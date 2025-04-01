2cd4b70dd1e1d45220645a816641e7ff2d725c41
alex17-T <a.senatore151@studenti.unisa.it>
2025-04-01 14:08:13 +0200

    elenco ag

--- Esercitazione_Agriturismi/src/classi/ElencoAgriturismi.java
+++ Esercitazione_Agriturismi/src/classi/ElencoAgriturismi.java
@@ -1,4 +1,59 @@
 package classi;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class ElencoAgriturismi {
+
+  private List<Agriturismo> elenco ;
+
+
+
+  public ElencoAgriturismi(){
+
+    elenco = new ArrayList<>();
+
+  }
+
+
+  public void aggiungi (Agriturismo a){
+
+    elenco.add(a);
+
+  }
+
+
+
+
+  public void esporta(){
+
+  }
+
+  public void filtra (Predicate){
+
+
+  }
+
+
+  public void ordina(){
+
+
+  }
+
+  public void aggiorna(){
+
+  }
+
+  public void somma(){
+
+  }
+
+
+  public void carica(){
+
+
+    
+  }
+
 }
