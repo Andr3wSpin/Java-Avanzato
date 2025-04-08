a639089f6145871f67b11c206700974b7375442d
Paolo <paolo32v@gmail.com>
2025-04-08 15:16:11 +0200

    ds

--- Es_Stream1/src/TestStream.java
+++ Es_Stream1/src/TestStream.java
@@ -10,22 +10,13 @@ public class TestStream {
     public static void main(String[] args) {
 
         ElencoAgriturismi elencoAgriturismi = ElencoAgriturismi.carica("Agriturismi-Benevento.csv");
-<<<<<<< HEAD
-=======
-
 
         System.out.println(elencoAgriturismi);
 
-        Map<String, Integer> postiXcomune =  elencoAgriturismi.stream().map(Agriturismo ::getComune);
-
-
-
-
-
-
->>>>>>> dc5b81b9e13bfbac8d54b8fe557e28a1f88e5150
-        System.out.println((elencoAgriturismi.elencoComuni()).toString());
-        //System.out.println(elencoAgriturismi);
+        //Map<String, Integer> postiXcomune =  elencoAgriturismi.stream().map(Agriturismo ::getComune);
+        Set<?> comuni = elencoAgriturismi.stream().map(Agriturismo::getComune).collect(Collectors.toSet());
+        System.out.println(comuni.toString());
+       // System.out.println(elencoAgriturismi);
 
     }
 }
