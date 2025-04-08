aaae132006b187cc198508f7dbfd67133817afe0
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-04-08 14:17:15 +0200

    classi agriturismo ed elenco ag

--- Es_Stream1/src/Agriturismo.java
+++ Es_Stream1/src/Agriturismo.java
@@ -1,2 +1,156 @@
-public class Agriturismo {
+package classi;
+
+import java.util.Objects;
+
+public class Agriturismo   {
+    private String comune;
+    private String titolare;
+    private String denominazione;
+    private String indirizzo;
+    private int postiLetto;
+    private int postiMacchina;
+    private int postiTenda;
+    private int postiRoulotte;
+    private String recapito;
+    private boolean pernottamento;
+    private boolean camping;
+
+    public Agriturismo(String comune, String titolare, String denominazione, String indirizzo, int postiLetto, int postiMacchina, int postiTenda, int postiRoulotte, String recapito) {
+        this.comune = comune;
+
+        this.titolare = titolare;
+
+        this.denominazione = denominazione;
+
+        this.indirizzo = indirizzo;
+
+        this.postiLetto = postiLetto;
+
+        this.postiMacchina = postiMacchina;
+
+        this.postiTenda = postiTenda;
+
+        this.postiRoulotte = postiRoulotte;
+
+        this.recapito = recapito;
+
+        this.pernottamento = false;
+
+        this.camping = false;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public String getTitolare() {
+        return titolare;
+    }
+
+    public void setTitolare(String titolare) {
+        this.titolare = titolare;
+    }
+
+    public String getDenominazione() {
+        return denominazione;
+    }
+
+    public void setDenominazione(String denominazione) {
+        this.denominazione = denominazione;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public int getPostiLetto() {
+        return postiLetto;
+    }
+
+    public void setPostiLetto(int postiLetto) {
+        this.postiLetto = postiLetto;
+    }
+
+    public int getPostiMacchina() {
+        return postiMacchina;
+    }
+
+    public void setPostiMacchina(int postiMacchina) {
+        this.postiMacchina = postiMacchina;
+    }
+
+    public int getPostiTenda() {
+        return postiTenda;
+    }
+
+    public void setPostiTenda(int postiTenda) {
+        this.postiTenda = postiTenda;
+    }
+
+    public int getPostiRoulotte() {
+        return postiRoulotte;
+    }
+
+    public void setPostiRoulotte(int postiRoulotte) {
+        this.postiRoulotte = postiRoulotte;
+    }
+
+    public boolean isPernottamento() { return pernottamento; }
+
+    public void setPernottamento(boolean pernottamento){ this.pernottamento = pernottamento; }
+
+    public boolean isCamping() {
+        return camping;
+    }
+
+    public void setCamping(boolean camping) {
+        this.camping = camping;
+    }
+
+    public String getRecapito() {
+        return recapito;
+    }
+
+    public void setRecapito(String recapito) {
+        this.recapito = recapito;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(denominazione + " di " + titolare + "\n" + indirizzo + "\n" + comune + "\n");
+        if(postiLetto > 0)
+            sb.append("Disponibili " + postiLetto + " posti letto.\n");
+        if(postiMacchina > 0)
+            sb.append("Disponibili posti " + postiMacchina + " auto.\n");
+        if(postiTenda > 0)
+            sb.append("Disponibili " + postiTenda + " posti tenda.\n");
+        if(postiRoulotte > 0)
+            sb.append("Disponibili " + postiRoulotte + " posti roulotte.\n");
+        sb.append(recapito + "\n");
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Agriturismo that = (Agriturismo) o;
+        return Objects.equals(denominazione, that.denominazione);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(denominazione);
+    }
 }
+
---
+++ Es_Stream1/src/ElencoAgriturismi.java
@@ -0,0 +1,123 @@
+package classi;
+
+
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class ElencoAgriturismi {
+
+  private List<Agriturismo> elenco ;
+
+  public ElencoAgriturismi() {
+
+    elenco = new ArrayList<>();
+  }
+
+
+  public void aggiungi(Agriturismo a) {
+
+    elenco.add(a);
+  }
+
+  public Set<String> esporta(Predicate<Agriturismo> filtroAgriturismo) {
+
+      Set<String> comuni = new HashSet<>();
+
+      for(Agriturismo a : elenco) {
+
+          if(filtroAgriturismo.test(a)) comuni.add(a.getComune());
+      }
+
+      return comuni;
+  }
+
+  public ElencoAgriturismi filtra (Predicate<Agriturismo> filtroAgriturismo) {
+
+     ElencoAgriturismi e = new ElencoAgriturismi();
+
+       for (Agriturismo a: elenco){
+
+         if (filtroAgriturismo.test(a))  e.elenco.add(a);
+       }
+
+        return e;
+  }
+
+  public void ordina(Comparator<Agriturismo> comparator) {
+
+      elenco.sort(comparator);
+
+  }
+
+  public void aggiorna(Consumer<Agriturismo> aggiorna) {
+
+        for (Agriturismo a: elenco) aggiorna.accept(a);
+  }
+
+  public int somma(Function<Agriturismo,Integer> sommatore){
+
+      int somma=0;
+
+       for (Agriturismo a : elenco){
+
+          somma += sommatore.apply(a);
+
+       }
+
+    return somma;
+
+  }
+
+  public static ElencoAgriturismi carica(String nomeFile) {
+
+      ElencoAgriturismi elencoAgriturismi = new ElencoAgriturismi();
+
+      try(BufferedReader br = new BufferedReader(new FileReader(nomeFile))) {
+
+          String line = br.readLine();
+
+          while((line = br.readLine()) != null) {
+
+              String[] campi = line.split(";");
+
+              String comuneAzienda = campi[0];
+              String titolare = campi[1];
+              String denominazioneAzienda = campi[2];
+              String indirizzoAzienda = campi[3];
+              int postiLetto = campi[4].isEmpty() ? 0 : Integer.parseInt(campi[4]);
+              int postiMacchina = campi[5].isEmpty() ? 0 : Integer.parseInt(campi[5]);
+              int postiTenda = campi[6].isEmpty() ? 0 : Integer.parseInt(campi[6]);
+              int postiRoulotte = campi[7].isEmpty() ? 0 : Integer.parseInt(campi[7]);
+              String recapiti = campi[8];
+
+              elencoAgriturismi.aggiungi(new Agriturismo(comuneAzienda, titolare, denominazioneAzienda,
+                      indirizzoAzienda, postiLetto, postiMacchina, postiTenda, postiRoulotte, recapiti));
+          }
+
+          return elencoAgriturismi;
+      } catch(IOException e) {
+
+          return null;
+      }
+  }
+
+  @Override
+  public String toString() {
+
+      StringBuilder sb = new StringBuilder();
+
+      for(Agriturismo agriturismo : elenco) sb.append(agriturismo + "\n");
+
+      return sb.toString();
+  }
+}
