dc5b81b9e13bfbac8d54b8fe557e28a1f88e5150
alex17-T <a.senatore151@studenti.unisa.it>
2025-04-08 14:55:29 +0200

    main map

--- Es_Stream1/src/TestStream.java
+++ Es_Stream1/src/TestStream.java
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import java.util.Map;
-=======
+
 import java.util.Set;
 import java.util.stream.Collectors;
->>>>>>> fee5debdc294095a44071f5376a2d82e693506be
+
 
 public class TestStream {
 
@@ -11,18 +11,18 @@ public class TestStream {
 
         ElencoAgriturismi elencoAgriturismi = ElencoAgriturismi.carica("Agriturismi-Benevento.csv");
 
-<<<<<<< HEAD
+
         System.out.println(elencoAgriturismi);
 
-        Map<String, Integer> postiXcomune =  elencoAgriturismi.stream().map(Agriturismo ::getComune)
+        Map<String, Integer> postiXcomune =  elencoAgriturismi.stream().map(Agriturismo ::getComune);
 
 
 
 
 
-=======
+
         System.out.println((elencoAgriturismi.elencoComuni()).toString());
         //System.out.println(elencoAgriturismi);
->>>>>>> fee5debdc294095a44071f5376a2d82e693506be
+
     }
 }
