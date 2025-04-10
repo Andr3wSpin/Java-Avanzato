02654cfe6faa7628280b50a217fc71f53cdc0674
Andr3wSpin <a.spinelli36@studenti.unisa.it>
2025-04-10 16:27:33 +0200

    .

--- Es_Stream1/src/TestStream.java
+++ Es_Stream1/src/TestStream.java
@@ -11,59 +11,54 @@ public class TestStream {
 //        stampaCollection(elenco);
         //ordinare alfabeticamnete
 
-
-
         //aggiorna il valore campeggio
-
-        List<Agriturismo> campeggi =       elenco.stream().filter(  f ->  f.getPostiRoulotte()>0 || f.getPostiTenda()>0 ).map( agriturismo -> {
+        List<Agriturismo> campeggi = elenco.stream().filter(f -> f.getPostiRoulotte() > 0 || f.getPostiTenda() > 0).map(agriturismo -> {
             agriturismo.setCamping(true);
-            return agriturismo;   }).collect(Collectors.toList());
-
+            return agriturismo;
+        }).collect(Collectors.toList());
 
         //System.out.println(campeggi);
 
-        //aggiorno pernottamneto
-
-        List<Agriturismo> aggiornaPernottamento  =  elenco.stream().filter(  f ->  f.getPostiLetto()>0 ).map( agriturismo -> {
-            agriturismo.setPernottamento(true);
-            return agriturismo;   }).collect(Collectors.toList());
+        //aggiorno pernottamento
+        List<Agriturismo> aggiornaPernottamento = elenco.stream().filter(f -> f.getPostiLetto() > 0).
+                map(agriturismo -> {
+                    agriturismo.setPernottamento(true);
+                    return agriturismo;
+                })
+                .collect(Collectors.toList());
         //System.out.println(elencoAggiornato);
 
-
-
         List<Agriturismo> elencoOrdinato = elenco.stream().sorted(
-                (a,b) -> a.getDenominazione().compareTo(b.getDenominazione())).toList();
+                (a, b) -> a.getDenominazione().compareTo(b.getDenominazione())).toList();
 
-      //  System.out.println("Elenco ordinato afabeticamente:");
-     //   stampaCollection(elencoOrdinato);
+        //  System.out.println("Elenco ordinato afabeticamente:");
+        //   stampaCollection(elencoOrdinato);
 
         //ottenere il comune con max posti camping
-        Optional<String> ComuneMaxPosti =  campeggi.stream().filter(f-> f.isCamping()==true ).reduce((agriturismo, agriturismo2) ->
-                agriturismo.getPostiCamping()>agriturismo2.getPostiCamping() ? agriturismo : agriturismo2
-        ).map(Agriturismo::getComune);
+        Optional<String> ComuneMaxPosti = campeggi.stream().filter(f -> f.isCamping() == true)
+                .reduce((agriturismo, agriturismo2) ->
+                        agriturismo.getPostiCamping() > agriturismo2.getPostiCamping() ? agriturismo : agriturismo2
+                ).map(Agriturismo::getComune);
 
-       // System.out.println(ComuneMaxPosti.get());
+        System.out.println(ComuneMaxPosti.get());
 
         //mappa comune e numero posti letto
-        Map<String, Integer> postiXcomune =  elenco.stream().collect( Collectors.groupingBy(
+        Map<String, Integer> postiXcomune = elenco.stream().collect(Collectors.groupingBy(
                 Agriturismo::getComune,
                 Collectors.summingInt(Agriturismo::getPostiLetto)
         ));
 
-      //  System.out.println(postiXcomune);
+        //  System.out.println(postiXcomune);
         // crea una mappa nome Comune numeroPosti camping
-        Map<String,Double> postiCamping = campeggi.stream().collect(Collectors.groupingBy(
+        Map<String, Double> postiCamping = campeggi.stream().collect(Collectors.groupingBy(
                 Agriturismo::getComune, Collectors.averagingInt(Agriturismo::getPostiCamping))
-
         );
 
         //lista comuni che ospitano agriturisimi
         Set<String> comuni = elenco.stream().map(Agriturismo::getComune).collect(Collectors.toSet());
 //        System.out.println("Lista dei comuni che ospitanto agriturismi:");
 //        stampaCollection(comuni);
-       // System.out.println(elencoAgriturismi);
-
-
+        // System.out.println(elencoAgriturismi);
     }
 
     private static void stampaCollection(Collection<?> collezione) {
