87efa08f23f15c7e406a1dfb12c3e14b97c76b25
Paolo <paolo32v@gmail.com>
2025-04-16 16:22:07 +0200

    sd

--- MyChatFX/src/main/java/mychat/mychatfx/MenuChatController.java
+++ MyChatFX/src/main/java/mychat/mychatfx/MenuChatController.java
@@ -1,4 +1,26 @@
 package mychat.mychatfx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 public class MenuChatController {
+
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField messageTF;
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private void sendMessage(KeyEvent event) {
+    }
+
+    @FXML
+    private void sendMs(ActionEvent event) {
+    }
 }
--- MyChatFX/src/main/java/mychat/mychatfx/MenuPrincipaleController.java
+++ MyChatFX/src/main/java/mychat/mychatfx/MenuPrincipaleController.java
@@ -1,12 +1,26 @@
 package mychat.mychatfx;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class MenuPrincipaleController {
 
     @FXML
+    private Button yesBtn;
+    @FXML
+    private Button noBtn;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void startServer(ActionEvent event) {
+    }
+
+    @FXML
+    private void startClient(ActionEvent event) {
     }
 }
